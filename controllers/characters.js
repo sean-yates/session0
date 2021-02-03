60ebfef8075ee8b0786f25565b54c3be165c7346
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-02-02 19:46:34 -0800

    delete router and controller set up

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,6 +4,7 @@ module.exports = {
     feed,
     newCharacter,
     create,
+    deleteCharacter,
   };
 
 // show all characters
@@ -25,9 +26,20 @@ function newCharacter(req, res) {
 
 // creation of new character
   function create(req,res){
+    req.body.user = req.user
     db.Character.create( req.body, (err, createdCharacter ) => {
       if (err) return console.log(err);
       
       res.redirect('/characters')
     });
+  }
+
+  // delete character
+
+  function deleteCharacter(req,res){
+    db.Character.deleteOne({ _id: req.params.id }, function (err) {
+      if(err) console.log(err);
+      console.log("Successful deletion");
+    });
+    res.redirect(`/characters`);
   }
--- routes/characters.js
+++ routes/characters.js
@@ -6,6 +6,6 @@ const { isLoggedIn } = require('../controllers/users');
 router.get('/characters', charactersCtrl.feed)
 router.get('/characters/new', isLoggedIn, charactersCtrl.newCharacter)
 router.post('/characters', charactersCtrl.create)
-
+router.delete('/characters/:id', charactersCtrl.deleteCharacter)
 
   module.exports = router;
