a0eec32b98d76a869593fb841e83f684037eb8ac
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-02-03 20:10:54 -0800

    added comment updates

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,6 +3,8 @@ const db = require('../models');
 module.exports = {
     createComment,
     deleteComment,
+    editComment,
+    updateComment,
   };
 
 
@@ -37,3 +39,44 @@ function deleteComment(req,res){
         res.redirect(`/characters/${req.params.id}`)
       });
 }
+
+
+
+// edit comment page
+function editComment(req,res){
+    db.Comment.findById(req.params.cid).
+    populate('user').
+    exec( function(err, currentComment) {
+      if(err) console.log(err);
+      res.render('editComment', {
+       currentCharacterId: req.params.id,
+       currentComment,
+       currentUser: req.user,
+       title: 'Edit Comment',
+       });
+    });
+  }
+
+// update comment
+
+
+function updateComment(req,res){
+    const id = req.params.cid;
+  
+    db.Comment.findByIdAndUpdate( 
+      id,
+      {
+        $set: {
+          ...req.body
+        },
+      },
+      {
+        new: true
+      },
+      ( err, updatedComment ) => {
+        if ( err ) return console.log( err );
+  
+        res.redirect(`/characters/${req.params.id}`)
+      });
+  }
+
--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,7 @@ const { isLoggedIn } = require('../controllers/users');
 // Comments
 router.delete('/characters/:id/comments/:cid',commentsCtrl.deleteComment)
 router.post('/characters/:id/comments',isLoggedIn, commentsCtrl.createComment)
+router.get('/characters/:id/comments/:cid/edit', commentsCtrl.editComment)
+router.put('/characters/:id/comments/:cid', commentsCtrl.updateComment)
 
 module.exports = router;
