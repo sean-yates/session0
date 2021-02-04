7c315328b7e349c2cb8995ba1612b387df47700c
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-02-03 19:50:33 -0800

    fixed delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,7 +11,6 @@ module.exports = {
 
   //create comment
   function createComment(req,res){
-    console.log('*****START OF CREATING A COMMENT******')
     req.body.user = req.user
     db.Comment.create( req.body, (err, createdComment ) => {
         if (err) return console.log(err);
@@ -32,19 +31,9 @@ module.exports = {
 // delete comment
 
 function deleteComment(req,res){
-    // none of this is valid anymore because of model switch
-//   db.Character.findById(req.params.id).
-//   exec( function(err, currentCharacter) {
-//     if(err) console.log(err);
-
-//     currentCharacter.comments = currentCharacter.comments.filter(function( comment ) {
-//       return comment._id !== req.params.cid;
-//     })
-//     // curently here, not working
-//     currentCharacter.save(function(err) {
-//         if(err) console.log(err);
-//         res.redirect(`/characters/${req.params.id}`)
-//       })
-//   });
-console.log('delete comments here')
+    db.Comment.deleteOne({ _id: req.params.cid }, function (err) {
+        if(err) console.log(err);
+        console.log("Successful deletion");
+        res.redirect(`/characters/${req.params.id}`)
+      });
 }
